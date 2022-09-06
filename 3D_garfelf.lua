c113f52bea60b6acba14c94c6905b351de6574c8
Ivory <88780282+IvoryTowr@users.noreply.github.com>
2022-09-06 21:11:26 +0300

    Update 3D_garfelf.lua

--- 3D_garfelf.lua
+++ 3D_garfelf.lua
@@ -8,3 +8,5 @@ function _draw()
 	sspr(0,0,8,8,64,4,width,res)
 	sspr(0,0,8,8,64,4,-width,res)
 end
+
+--link (for loading garfelf's sprite): https://www.pico-8-edu.com/?c=AHB4YQC9AI5PcPYbHHTFGWfsBsERQXb7U5Qr0Rs0wenxJXV9TRz0b-AKpxcDzSNMmGohOP8Zovv9dP1EHD-EPQe1o_UrTGVn_SCdykYWRsogXLtqauo0jyVJU1hjIhxp32AuVRcYiqti5iyBoaIQHtjZ2QkGdyQD0oUNB5x2WlMUakGBOVr3f4P2z9TABg==&g=QZw-w2JBJw-w15Aw-w1JBJBw-w1JBZw-w1pBw-w1OKZw-w1KZB
